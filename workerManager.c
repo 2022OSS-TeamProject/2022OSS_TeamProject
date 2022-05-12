b64aa0aea57dc7c7c2732e48978612258ca7c6bb
Youngkwan_Cho <100776540+Youngkwan-Cho@users.noreply.github.com>
2022-05-12 20:58:31 +0900

    Debug workerManager.c

--- workerManager.c
+++ workerManager.c
@@ -69,7 +69,7 @@ void searchWorkerPosition(worker *s, int count){
     }
     printf("\n총 %d명의 직원이 있습니다.\n", scnt);
     if (scnt==0) printf("=> 검색된 데이터 없음!\n");   
-}
+} 
 
 void searchWorkerTotalTime(worker *s, int count){
     int scnt=0;
